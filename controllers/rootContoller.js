6309c8a251e704bd0fc35f104fdc95766a15c0af
MekkyMayata <okpallannaemeka@gmail.com>
2020-04-08 15:16:50 +0100

    test branch-view-update

--- controllers/rootContoller.js
+++ controllers/rootContoller.js
@@ -1,51 +1,63 @@
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const product = require('../models/article')
+const article = require('../models/article')
 
 //dummy data
-let data = [{item:'You have fun, we\'ll handle the baking...'}, {
-    item: 'Welcome to the Edibles bakery an italian inspired bakery shop located in the heart of Lagos, at the Ikeja city mall, and at various shopping malls around the west-side.'}]
+// let data = [{item:'You have fun, we\'ll handle the baking...'}, {item: 'alice'}]
 
 //fire product model
-product()
+article()
 
 module.exports = function(app) {
-
     //routing
+
+    //home page
     app.get('/edibles/home', function(req, res) {
         //fetching data from the database
-        product.Product.find({}, (err, data) => {
+        article.Product.find({}, (err, data) => {
+            console.log(data);
             if(err) throw err;
             res.render('home', {products: data});
         })
         
     })
 
+    //admin page
     app.get('/edibles/admin', function(req, res) {})
 
+    //about page
     app.get('/edibles/about', function(req,res) {
         res.render('about', {content: data});
     })
 
+    //contact page
     app.get('edibles/location', function(req, res) {})
 
+    //signup
     app.get('edibles/signup', function(req, res) {})
 
+    //update form
     app.get('/edibles/updates', function(req, res) {
         res.render('updates', {qs: req.query});
         console.log(req.query)
     });
 
+
     app.post('/edibles/content', urlencodedParser, (req, res) => {
         console.log(req.body);
 
-        let newProduct = product.Product(req.body).save((err, data) => {
-            if(err) throw err;
-            
+        let newProduct = article.Product(req.body).save();
+        article.Product.find({}, (err,data) => {
+            if (err) {
+                throw err;
+            } else {
+            res.render('home', {products: data});
+            }
         })
-        res.render('home', {content: data});
+        
     });
 
+    //sessions
     app.post('/edibles/admin/login', function(req, res) {});
 
     
