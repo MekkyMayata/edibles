b0177bb86fa7c7c8cbd416d80d9b7f9ad80e0344
MekkyMayata <okpallannaemeka@gmail.com>
2020-07-02 02:30:47 +0100

    update app structure

--- src/models/article.js
+++ src/models/article.js
@@ -1,32 +1,35 @@
 //require mongoose
-let mongoose = require('mongoose')
+let mongoose = require('mongoose');
+let config = require('../config');
+
+const { db } = config;
 
 module.exports = function() {
 //create a connection 
-mongoose.connect('mongodb+srv://MekkyMayata:42toO5JmJPgmq5cr@ediblescluster-g5ret.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('db server connected'))
-    .catch((err) => console.log('db error'))
+    .catch((err) => console.log('db error'));
 
 mongoose.connection.once('open', function() {
-    console.log('Connection made')
+    console.log('Connection made');
 }).on('error', function(error) {
     console.log('Connection failed due to: \n', error);
-})
+});
 
 
 
-}
+};
 
 //create a schema for the model/collection
 const productSchema = new mongoose.Schema({
     product: String,
     description: String
-})
+});
 
 //create the model
 const Product = mongoose.model('Product', productSchema);
 
-module.exports.ProductSchema = productSchema
-module.exports.Product = Product
+module.exports.ProductSchema = productSchema;
+module.exports.Product = Product;
